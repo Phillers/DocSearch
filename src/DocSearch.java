72160d1413ade195253faefe38e0fc4dd9f22037
Phillers <phillers95@gmail.com>
2018-10-13 15:20:14 +0200

    start

---
+++ src/DocSearch.java
@@ -0,0 +1,236 @@
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
+
+class GUI implements Runnable{
+    private DocSearch home;
+    private List<Document> documents;
+    private JTable table = new JTable();
+    private JTextArea content = new JTextArea();
+
+    @Override
+    public void run() {
+        JFrame jfrm = new JFrame("Document Search");
+        jfrm.setSize(1000, 600);
+        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jfrm.setLayout(new GridBagLayout());
+        JLabel lbl1 = new JLabel("Keywords file:");
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        c.weighty = 1;
+        jfrm.add(lbl1, c);
+        JTextField jtx1 = new JTextField("keywords.txt");
+        c.gridx = 1;
+        c.weightx = 2;
+        jfrm.add(jtx1, c);
+        JLabel lbl2 = new JLabel("Documents file:");
+        c.gridx = 2;
+        c.weightx = 0.5;
+        jfrm.add(lbl2, c);
+        JTextField jtx2 = new JTextField("documents.txt");
+        c.gridx = 3;
+        c.weightx = 2;
+        jfrm.add(jtx2, c);
+        JButton btn1 = new JButton("Open");
+        btn1.addActionListener(e -> getDatabase(jtx1.getText(), jtx2.getText()));
+        c.gridx = 4;
+        c.weightx = 0.5;
+        jfrm.add(btn1, c);
+        JLabel lbl3 = new JLabel("Query text:");
+        c.gridx = 5;
+        c.weightx = 0.5;
+        jfrm.add(lbl3, c);
+        JTextField jtx3 = new JTextField("", 50);
+        c.gridx = 6;
+        c.weightx = 3;
+        jfrm.add(jtx3, c);
+        JButton btn2 = new JButton("Search");
+        c.gridx = 7;
+        c.weightx = 0.5;
+        btn2.addActionListener(e -> search(jtx3.getText()));
+        jfrm.add(btn2, c);
+        JScrollPane sp1 = new JScrollPane();
+        c.weightx = 0;
+        c.weighty = 3;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 5;
+        //c.gridheight = 3;
+        c.fill = GridBagConstraints.BOTH;
+        jfrm.add(sp1, c);
+        table.setDefaultEditor(Object.class, null);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        ListSelectionModel selectionModel = table.getSelectionModel();
+        selectionModel.addListSelectionListener(e -> {
+            if(!e.getValueIsAdjusting()) {
+                showDocument(((ListSelectionModel)e.getSource()).getMinSelectionIndex());
+            }
+        });
+        sp1.getViewport().add(table);
+        JScrollPane sp2 = new JScrollPane();
+        c.weightx = 0;
+        c.gridx = 5;
+        c.gridy = 1;
+        c.gridwidth = 3;
+        c.gridheight = 3;
+        sp2.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        jfrm.add(sp2, c);
+        content.setLineWrap(true);
+        content.setWrapStyleWord(true);
+        content.setEditable(false);
+        sp2.getViewport().add(content);
+        jfrm.setVisible(true);
+    }
+
+    private void search(String expression) {
+        documents = new ArrayList<>(home.search(expression));
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Document title");
+        model.addColumn("Similarity");
+        for ( Document doc : documents) {
+            model.addRow(new Object[]{doc.title, doc.s});
+        }
+        table.setModel(model);
+        model.fireTableDataChanged();
+    }
+
+    private void showDocument(int index) {
+        try {
+            content.setText(documents.get(index).content);
+        } catch(ArrayIndexOutOfBoundsException e){
+            content.setText("");
+        }
+    }
+
+    private void getDatabase(String keywords, String documentFile) {
+        documents = new ArrayList<>(home.prepareDb(keywords, documentFile));
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Document title");
+        for ( Document doc : documents) {
+            model.addRow(new Object[]{doc.title});
+        }
+        table.setModel(model);
+    }
+
+
+    GUI(DocSearch home){
+        this.home = home;
+    }
+}
+public class DocSearch {
+
+    private TreeMap<String, Double> keywords = new TreeMap<>();
+    private Vector<Document> documents = new Vector<>();
+
+    public static void main(String[] args) {
+        DocSearch ds = new DocSearch();
+        GUI gui = new GUI(ds);
+        SwingUtilities.invokeLater(gui);
+        //ds.prepareDb("keywords.txt", "documents.txt");
+        //Vector<Document> res = ds.search("machine learning");
+        //for (Document doc : res){
+        //    System.out.println(doc.s +" "+ doc.title);
+        //}
+    }
+
+    Vector<Document> search(String expression) {
+        Vector<Document> res = new Vector<>();
+        Document query = new Document();
+        query.processContent(expression);
+        query.calculateTF(keywords, 0);
+        query.calculateTFIDF(keywords);
+        for (Document doc : documents){
+            doc.calculateSimilarity(query);
+            addToResultSet(res, doc);
+        }
+        return res;
+    }
+
+    private void addToResultSet(Vector<Document> set, Document doc) {
+        if(doc.s > 0 && set.size()>0 ){
+            for(int i=0; i<set.size(); i++){
+                if(doc.s > set.elementAt(i).s){
+                    set.insertElementAt(doc, i);
+                    return;
+                }
+            }
+        }
+        set.add(doc);
+    }
+
+
+    Vector<Document> prepareDb(String keyFile, String docFile) {
+        readFiles(keyFile, docFile);
+        calculate();
+        return documents;
+    }
+
+    private void calculate() {
+        for (Document doc : documents){
+            doc.calculateTF(keywords,1);
+        }
+        int count = documents.size();
+        keywords.replaceAll((k, v) -> Math.log10(count/v));
+        for (Document doc : documents){
+            doc.calculateTFIDF(keywords);
+        }
+    }
+
+    private void readFiles(String keyFile, String docFile) {
+        keywords.clear();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(keyFile));
+            while (br.ready()) {
+                String doc = br.readLine().trim();
+                Stemmer s = new Stemmer();
+                s.add(doc.toCharArray(), doc.length());
+                s.stem();
+                doc = s.toString();
+                keywords.put(doc, .0);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No keyword database available.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        documents.clear();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(docFile));
+            while (br.ready()) {
+                StringBuilder str = new StringBuilder(br.readLine().trim());
+                Document doc = new Document();
+                doc.title = str.toString();
+                str.append('\n');
+                String str2;
+                while(br.ready() &&(str2 = br.readLine().trim()).length() >0){
+                    str.append(' ').append(str2);
+                }
+                doc.processContent(str.toString());
+                documents.add(doc);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No document database available.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/Document.java
@@ -0,0 +1,71 @@
+import java.util.TreeMap;
+import javax.swing.*;
+class Document{
+    String title;
+    String content;
+    private String processedContent;
+    private TreeMap<String, Double> words = new TreeMap<>();
+    private Double d;
+    Double s;
+
+    void calculateTF(TreeMap<String, Double> keywords, int change){
+        String[] wordsArr = processedContent.split(" +");
+        Double count;
+        Double sum = .0;
+        for (String word : wordsArr){
+            if((count = words.get(word)) != null) {
+                words.put(word, count + 1);
+                sum += 1;
+            }
+            else if((count = keywords.get(word)) != null){
+                keywords.put(word, count + change);
+                words.put(word, 1.0);
+                sum += 1;
+            }
+        }
+        for ( String key : words.keySet()){
+            words.put(key, words.get(key)/sum);
+        }
+    }
+
+    void calculateTFIDF(TreeMap<String, Double> keywords){
+        double sum = .0;
+        for(String key : words.keySet()){
+            Double val = words.get(key) * keywords.get(key);
+            words.put(key, val);
+            sum += (val * val);
+        }
+        d = Math.sqrt(sum);
+    }
+
+    void processContent(String str) {
+        content = str;
+        str = str.toLowerCase();
+        str = str.replaceAll("[^\\p{IsAlphabetic}\\p{IsDigit} ]", "");
+        String[] words = str.split(" ");
+        for(int i = 0 ; i < words.length; i++){
+            Stemmer s = new Stemmer();
+            s.add(words[i].toCharArray(), words[i].length());
+
+            s.stem();
+            words[i] = s.toString();
+        }
+        str = String.join(" ", words);
+        processedContent = str;
+    }
+
+
+
+    void calculateSimilarity(Document query) {
+        double sum = .0;
+        for ( String key : query.words.keySet()) {
+            Double a;
+            if ((a =  words.get(key))!= null )
+                sum += query.words.get(key) *a;
+        }
+        if(d == 0 || query.d == 0)
+            s = .0;
+        else
+            s = sum/(d*query.d);
+    }
+}
